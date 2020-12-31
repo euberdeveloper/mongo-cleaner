a41be0c7629c4067d6922b8ed5ff21b06893a8f1
euberdeveloper <euberdeveloper@gmail.com>
2020-12-31 19:05:07 +0100

    docs: add properly typedoc

---
+++ typedoc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entryPoints: [
+        './source/lib/index.ts'
+    ],
+    name: 'mongo-cleaner',
+    excludeExternals: true,
+    includeVersion: true,
+    tsconfig: 'source/tsconfig.json',
+    gaID: process.env.GA_TOKEN,
+    excludePrivate: true,
+    excludeProtected: true,
+    disableSources: true,
+    out: './docs/documentation/html'
+};
