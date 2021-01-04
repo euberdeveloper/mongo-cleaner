66ca99dfc805c03ad526cd99433d40cce4031dc7
euberdeveloper <euberdeveloper@gmail.com>
2021-01-04 12:54:17 +0100

    test(logger.test.js): add stopDropDatabase tests

--- test/logger.test.js
+++ test/logger.test.js
@@ -4,6 +4,8 @@ module.exports = (expect, sinon) => {
 
     describe('Test: logger', function () {
 
+        /* startDropDatabase */
+
         it(`Should properly execute startDropDatabase with log enabled`, function () {
             const stubStart = sinon.stub();
             const stubOra = sinon.stub().returns({
@@ -17,7 +19,7 @@ module.exports = (expect, sinon) => {
             const logger = new Logger({ log: true });
             logger.startDropDatabase('database');
 
-            expect(stubOra).to.have.been.calledOnceWith(sinon.match({
+            expect(stubOra).to.have.been.calledOnceWithExactly(sinon.match({
                 text: `Dropping database`,
                 spinner: 'dots2'
             }));
@@ -35,6 +37,129 @@ module.exports = (expect, sinon) => {
 
             expect(stubOra).to.have.callCount(0);
         });
+
+        /* stopDropDatabase */
+
+        it(`Should properly execute stopDropDatabase with succeded`, function () {
+            const stubSucceeded = sinon.stub();
+            const stubWarn = sinon.stub();
+            const stubFail = sinon.stub();
+            const stubStart = sinon.stub().returns({
+                succeed: stubSucceeded,
+                warn: stubWarn,
+                fail: stubFail
+            });
+            const stubOra = sinon.stub().returns({
+                start: stubStart
+            });
+
+            const { Logger } = proxyquire('../dist/lib/utils/logger', {
+                ora: stubOra
+            });
+
+            const logger = new Logger({ log: true });
+            logger.startDropDatabase('database');
+            logger.stopDropDatabase(true, false);
+            logger.stopDropDatabase(true, true);
+
+            expect(stubOra).to.have.been.calledOnceWithExactly(sinon.match({
+                text: `Dropping database`,
+                spinner: 'dots2'
+            }));
+            expect(stubStart).to.have.been.calledOnce;
+            expect(stubSucceeded).to.have.been.calledTwice;
+            expect(stubFail).to.have.callCount(0);
+            expect(stubWarn).to.have.callCount(0);
+        });
+        it(`Should properly execute stopDropDatabase with fallback`, function () {
+            const stubSucceeded = sinon.stub();
+            const stubWarn = sinon.stub();
+            const stubFail = sinon.stub();
+            const stubStart = sinon.stub().returns({
+                succeed: stubSucceeded,
+                warn: stubWarn,
+                fail: stubFail
+            });
+            const stubOra = sinon.stub().returns({
+                start: stubStart
+            });
+
+            const { Logger } = proxyquire('../dist/lib/utils/logger', {
+                ora: stubOra
+            });
+
+            const logger = new Logger({ log: true });
+            logger.startDropDatabase('database');
+            logger.stopDropDatabase(true, true);
+            logger.stopDropDatabase(false, true);
+
+            expect(stubOra).to.have.been.calledOnceWithExactly(sinon.match({
+                text: `Dropping database`,
+                spinner: 'dots2'
+            }));
+            expect(stubStart).to.have.been.calledOnce;
+            expect(stubSucceeded).to.have.been.calledOnce;
+            expect(stubFail).to.have.callCount(0);
+            expect(stubWarn).to.have.been.calledOnce;
+        });
+        it(`Should properly execute stopDropDatabase without fallback`, function () {
+            const stubSucceeded = sinon.stub();
+            const stubWarn = sinon.stub();
+            const stubFail = sinon.stub();
+            const stubStart = sinon.stub().returns({
+                succeed: stubSucceeded,
+                warn: stubWarn,
+                fail: stubFail
+            });
+            const stubOra = sinon.stub().returns({
+                start: stubStart
+            });
+
+            const { Logger } = proxyquire('../dist/lib/utils/logger', {
+                ora: stubOra
+            });
+
+            const logger = new Logger({ log: true });
+            logger.startDropDatabase('database');
+            logger.stopDropDatabase(true, false);
+            logger.stopDropDatabase(false, false);
+
+            expect(stubOra).to.have.been.calledOnceWithExactly(sinon.match({
+                text: `Dropping database`,
+                spinner: 'dots2'
+            }));
+            expect(stubStart).to.have.been.calledOnce;
+            expect(stubSucceeded).to.have.been.calledOnce;
+            expect(stubWarn).to.have.callCount(0);
+            expect(stubFail).to.have.been.calledOnce;
+        });
+        it(`Should properly execute stopDropDatabase with log disabled`, function () {
+            const stubSucceeded = sinon.stub();
+            const stubWarn = sinon.stub();
+            const stubFail = sinon.stub();
+            const stubStart = sinon.stub().returns({
+                succeed: stubSucceeded,
+                warn: stubWarn,
+                fail: stubFail
+            });
+            const stubOra = sinon.stub().returns({
+                start: stubStart
+            });
+
+            const { Logger } = proxyquire('../dist/lib/utils/logger', {
+                ora: stubOra
+            });
+
+            const logger = new Logger({ log: false });
+            logger.startDropDatabase('database');
+            logger.stopDropDatabase(true);
+
+            expect(stubOra).to.have.callCount(0);
+            expect(stubStart).to.have.callCount(0);
+            expect(stubSucceeded).to.have.callCount(0);
+        });
+
+        
         
     });
 
