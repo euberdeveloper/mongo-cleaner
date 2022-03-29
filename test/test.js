75c291b339891933b2476367e4d05e37a52b15f5
euberdeveloper <euberdeveloper@gmail.com>
2022-03-29 23:40:15 +0200

    .
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 import * as mongoclean from '../dist/lib/index.js';
 import mongodb from 'mongodb';
-import sinon from 'sinon';
 import { execSync } from 'child_process';
 
 import chai from 'chai';
@@ -11,7 +10,7 @@ import cleanTest from'./clean.test.js';
 import errorsTest from'./errors.test.js';
 import optionsTest from'./options.test.js';
 
-
+// TODO: Re-add tests that used mocking
 describe('MongoBack module tests', async function() {
     cleanTest(chai.expect, mongodb, execSync, mongoclean);
     errorsTest(chai.expect, mongoclean);
