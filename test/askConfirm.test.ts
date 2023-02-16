451517ea73e6f0d0ec6731f3bac638064302bbab
euberdeveloper <euberdeveloper@gmail.com>
2023-02-16 22:51:21 +0100

    test: add todo tests to reestablish
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ test/askConfirm.test.ts
@@ -0,0 +1,38 @@
+describe('Test: askConfirm', function () {
+    it.skip(`TODO: reestablish these tests`, function () {
+        console.log('skipped');
+    });
+    // it(`Should ask confirm and return true`, async function () {
+    //     const stubInquirer = sinon.stub().resolves({ clean: true });
+    //     const { askConfirm } = proxyquire('../dist/lib/utils/askConfirm', {
+    //         inquirer: {
+    //             prompt: stubInquirer
+    //         }
+    //     });
+    //     const result = await askConfirm(true);
+    //     expect(result).to.equal(true);
+    //     expect(stubInquirer).to.have.been.calledOnce;
+    // });
+    // it(`Should ask confirm and return false`, async function () {
+    //     const stubInquirer = sinon.stub().resolves({ clean: false });
+    //     const { askConfirm } = proxyquire('../dist/lib/utils/askConfirm', {
+    //         inquirer: {
+    //             prompt: stubInquirer
+    //         }
+    //     });
+    //     const result = await askConfirm(true);
+    //     expect(result).to.equal(false);
+    //     expect(stubInquirer).to.have.been.calledOnce;
+    // });
+    // it(`Should not ask confirm and return true`, async function () {
+    //     const stubInquirer = sinon.stub().resolves({ clean: true });
+    //     const { askConfirm } = proxyquire('../dist/lib/utils/askConfirm', {
+    //         inquirer: {
+    //             prompt: stubInquirer
+    //         }
+    //     });
+    //     const result = await askConfirm();
+    //     expect(result).to.equal(true);
+    //     expect(stubInquirer).to.have.not.been.called;
+    // });
+});
