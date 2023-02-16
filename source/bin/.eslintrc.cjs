8bfaca3fdc8fe007ad109e5decafc66ea4e9a0f1
euberdeveloper <euberdeveloper@gmail.com>
2023-02-16 01:29:13 +0100

    style: deactivate provvisory tsignore lint rule
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/bin/.eslintrc.cjs
+++ source/bin/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
     rules: {
         '@typescript-eslint/no-misused-promises': 'off',
-        '@typescript-eslint/no-floating-promises': 'off'
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off'
     }
 };
