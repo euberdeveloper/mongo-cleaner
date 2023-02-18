bb2b04c7a02e4a4fa943c925bc2221ae6184517c
euberdeveloper <euberdeveloper@gmail.com>
2023-02-19 00:13:24 +0100

    test(logger.test): add all the tests
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- test/logger.test.ts
+++ test/logger.test.ts
@@ -1,4 +1,4 @@
-import { mockOra, mockOraStart, mockOraSucceed, mockOraWarn, mockOraFail } from '@test/utils/mockOra.js';
+import { mockOra, mockOraStart, mockOraSucceed, mockOraWarn, mockOraFail, mockOraStop } from '@test/utils/mockOra.js';
 
 import { mergeOptions } from '@lib/utils/options.js';
 import { Logger } from '@lib/utils/logger.js';
@@ -16,6 +16,7 @@ describe('Test: logger', function () {
         mockOraFail.mockClear();
         mockOraWarn.mockClear();
         mockOraStart.mockClear();
+        mockOraStop.mockClear();
         mockOra.mockClear();
     });
 
@@ -24,6 +25,7 @@ describe('Test: logger', function () {
         mockOraSucceed.mockRestore();
         mockOraFail.mockRestore();
         mockOraWarn.mockRestore();
+        mockOraStop.mockRestore();
         mockOraStart.mockRestore();
         mockOra.mockRestore();
     });
@@ -134,275 +136,154 @@ describe('Test: logger', function () {
         expect(mockOra).toHaveBeenCalledTimes(1);
         expect(mockOraStart).toHaveBeenCalledTimes(1);
     });
-    // it(`Should properly execute startDropCollection with log disabled`, function () {
-    //     const stubOra = sinon.stub();
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: false });
-    //     logger.startDropCollection('collection');
-    //     expect(stubOra).not.toHaveBeenCalled();
-    // });
-    // /* stopDropCollection */
-    // it(`Should properly execute stopDropCollection with succeded`, function () {
-    //     const stubSucceeded = sinon.stub();
-    //     const stubWarn = sinon.stub();
-    //     const stubFail = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         succeed: stubSucceeded,
-    //         warn: stubWarn,
-    //         fail: stubFail
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: true });
-    //     logger.startDropCollection('collection');
-    //     logger.stopDropCollection(true, false);
-    //     logger.stopDropCollection(true, true);
-    //     expect(stubOra).to.have.been.calledOnceWithExactly(
-    //         sinon.match({
-    //             text: `Dropping collection`,
-    //             spinner: 'dots2'
-    //         })
-    //     );
-    //     expect(stubStart).to.have.been.calledOnce;
-    //     expect(stubSucceeded).to.have.been.calledTwice;
-    //     expect(stubFail).not.toHaveBeenCalled();
-    //     expect(stubWarn).not.toHaveBeenCalled();
-    // });
-    // it(`Should properly execute stopDropCollection with fallback`, function () {
-    //     const stubSucceeded = sinon.stub();
-    //     const stubWarn = sinon.stub();
-    //     const stubFail = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         succeed: stubSucceeded,
-    //         warn: stubWarn,
-    //         fail: stubFail
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: true });
-    //     logger.startDropCollection('collection');
-    //     logger.stopDropCollection(true, true);
-    //     logger.stopDropCollection(false, true);
-    //     expect(stubOra).to.have.been.calledOnceWithExactly(
-    //         sinon.match({
-    //             text: `Dropping collection`,
-    //             spinner: 'dots2'
-    //         })
-    //     );
-    //     expect(stubStart).to.have.been.calledOnce;
-    //     expect(stubSucceeded).to.have.been.calledOnce;
-    //     expect(stubFail).not.toHaveBeenCalled();
-    //     expect(stubWarn).to.have.been.calledOnce;
-    // });
-    // it(`Should properly execute stopDropCollection without fallback`, function () {
-    //     const stubSucceeded = sinon.stub();
-    //     const stubWarn = sinon.stub();
-    //     const stubFail = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         succeed: stubSucceeded,
-    //         warn: stubWarn,
-    //         fail: stubFail
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: true });
-    //     logger.startDropCollection('collection');
-    //     logger.stopDropCollection(true, false);
-    //     logger.stopDropCollection(false, false);
-    //     expect(stubOra).to.have.been.calledOnceWithExactly(
-    //         sinon.match({
-    //             text: `Dropping collection`,
-    //             spinner: 'dots2'
-    //         })
-    //     );
-    //     expect(stubStart).to.have.been.calledOnce;
-    //     expect(stubSucceeded).to.have.been.calledOnce;
-    //     expect(stubWarn).not.toHaveBeenCalled();
-    //     expect(stubFail).to.have.been.calledOnce;
-    // });
-    // it(`Should properly execute stopDropCollection with log disabled`, function () {
-    //     const stubSucceeded = sinon.stub();
-    //     const stubWarn = sinon.stub();
-    //     const stubFail = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         succeed: stubSucceeded,
-    //         warn: stubWarn,
-    //         fail: stubFail
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: false });
-    //     logger.startDropCollection('collection');
-    //     logger.stopDropCollection(true);
-    //     expect(stubOra).not.toHaveBeenCalled();
-    //     expect(stubStart).not.toHaveBeenCalled();
-    //     expect(stubSucceeded).not.toHaveBeenCalled();
-    //     expect(stubWarn).not.toHaveBeenCalled();
-    //     expect(stubFail).not.toHaveBeenCalled();
-    // });
-    // /* startEmptyCollection */
-    // it(`Should properly execute startEmptyCollection with log enabled`, function () {
-    //     const stubStart = sinon.stub();
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: true });
-    //     logger.startEmptyCollection('collection');
-    //     expect(stubOra).to.have.been.calledOnceWithExactly(
-    //         sinon.match({
-    //             text: `Emptying collection`,
-    //             spinner: 'dots2'
-    //         })
-    //     );
-    //     expect(stubStart).to.have.been.calledOnce;
-    // });
-    // it(`Should properly execute startEmptyCollection with log disabled`, function () {
-    //     const stubOra = sinon.stub();
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: false });
-    //     logger.startEmptyCollection('collection');
-    //     expect(stubOra).not.toHaveBeenCalled();
-    // });
-    // /* stopEmptyCollection */
-    // it(`Should properly execute stopEmptyCollection with succeded`, function () {
-    //     const stubSucceeded = sinon.stub();
-    //     const stubFail = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         succeed: stubSucceeded,
-    //         fail: stubFail
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: true });
-    //     logger.startEmptyCollection('collection');
-    //     logger.stopEmptyCollection(true);
-    //     expect(stubOra).to.have.been.calledOnceWithExactly(
-    //         sinon.match({
-    //             text: `Emptying collection`,
-    //             spinner: 'dots2'
-    //         })
-    //     );
-    //     expect(stubStart).to.have.been.calledOnce;
-    //     expect(stubSucceeded).to.have.been.calledOnce;
-    //     expect(stubFail).not.toHaveBeenCalled();
-    // });
-    // it(`Should properly execute stopEmptyCollection with failed`, function () {
-    //     const stubSucceeded = sinon.stub();
-    //     const stubFail = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         succeed: stubSucceeded,
-    //         fail: stubFail
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: true });
-    //     logger.startEmptyCollection('collection');
-    //     logger.stopEmptyCollection(false);
-    //     expect(stubOra).to.have.been.calledOnceWithExactly(
-    //         sinon.match({
-    //             text: `Emptying collection`,
-    //             spinner: 'dots2'
-    //         })
-    //     );
-    //     expect(stubStart).to.have.been.calledOnce;
-    //     expect(stubSucceeded).not.toHaveBeenCalled();
-    //     expect(stubFail).to.have.been.calledOnce;
-    // });
-    // it(`Should properly execute stopEmptyCollection with log disabled`, function () {
-    //     const stubSucceeded = sinon.stub();
-    //     const stubFail = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         succeed: stubSucceeded,
-    //         fail: stubFail
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: false });
-    //     logger.startEmptyCollection('collection');
-    //     logger.stopEmptyCollection(true);
-    //     expect(stubOra).not.toHaveBeenCalled();
-    //     expect(stubStart).not.toHaveBeenCalled();
-    //     expect(stubSucceeded).not.toHaveBeenCalled();
-    //     expect(stubFail).not.toHaveBeenCalled();
-    // });
-    // /* stopAndClear */
-    // it(`Should properly execute stopAndClear with logs enabled`, function () {
-    //     const stubStop = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         stop: stubStop
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: true });
-    //     logger.startDropDatabase('database');
-    //     logger.stopAndClear();
-    //     logger.startDropCollection('collection');
-    //     logger.stopAndClear();
-    //     logger.startEmptyCollection('collection');
-    //     logger.stopAndClear();
-    //     expect(stubOra).to.have.been.calledThrice;
-    //     expect(stubStart).to.have.been.calledThrice;
-    //     expect(stubStop).to.have.been.calledThrice;
-    // });
-    // it(`Should properly execute stopAndClear with logs disabled`, function () {
-    //     const stubStop = sinon.stub();
-    //     const stubStart = sinon.stub().returns({
-    //         stop: stubStop
-    //     });
-    //     const stubOra = sinon.stub().returns({
-    //         start: stubStart
-    //     });
-    //     const { Logger } = proxyquire('../dist/lib/utils/logger', {
-    //         ora: stubOra
-    //     });
-    //     const logger = new Logger({ log: false });
-    //     logger.startDropDatabase('database');
-    //     logger.stopAndClear();
-    //     logger.startDropCollection('collection');
-    //     logger.stopAndClear();
-    //     logger.startEmptyCollection('collection');
-    //     logger.stopAndClear();
-    //     expect(stubOra).not.toHaveBeenCalled();
-    //     expect(stubStart).not.toHaveBeenCalled();
-    //     expect(stubStop).not.toHaveBeenCalled();
-    // });
+
+    it(`Should properly execute startDropCollection with log disabled`, function () {
+        const logger = new Logger(mergeOptions({ log: false }));
+        logger.startDropCollection('collection');
+        expect(mockOra).not.toHaveBeenCalled();
+    });
+
+    /* stopDropCollection */
+
+    it(`Should properly execute stopDropCollection with succeded`, function () {
+        const logger = new Logger(mergeOptions({ log: true }));
+        logger.startDropCollection('collection');
+        logger.stopDropCollection(true, false);
+        logger.stopDropCollection(true, true);
+
+        expect(mockOraStart).toHaveBeenCalledTimes(1);
+        expect(mockOraSucceed).toHaveBeenCalledTimes(2);
+        expect(mockOraFail).not.toHaveBeenCalled();
+        expect(mockOraWarn).not.toHaveBeenCalled();
+    });
+
+    it(`Should properly execute stopDropCollection with fallback`, function () {
+        const logger = new Logger(mergeOptions({ log: true }));
+        logger.startDropCollection('collection');
+        logger.stopDropCollection(true, true);
+        logger.stopDropCollection(false, true);
+        expect(mockOra).toHaveBeenCalledTimes(1);
+        expect(mockOra).toHaveBeenCalledWith({
+            text: `Dropping collection`,
+            spinner: 'dots2'
+        });
+        expect(mockOraStart).toHaveBeenCalledTimes(1);
+        expect(mockOraSucceed).toHaveBeenCalledTimes(1);
+        expect(mockOraFail).not.toHaveBeenCalled();
+        expect(mockOraWarn).toHaveBeenCalledTimes(1);
+    });
+
+    it(`Should properly execute stopDropCollection without fallback`, function () {
+        const logger = new Logger(mergeOptions({ log: true }));
+        logger.startDropCollection('collection');
+        logger.stopDropCollection(true, false);
+        logger.stopDropCollection(false, false);
+        expect(mockOra).toHaveBeenCalledTimes(1);
+        expect(mockOra).toHaveBeenCalledWith({
+            text: `Dropping collection`,
+            spinner: 'dots2'
+        });
+        expect(mockOraStart).toHaveBeenCalledTimes(1);
+        expect(mockOraSucceed).toHaveBeenCalledTimes(1);
+        expect(mockOraWarn).not.toHaveBeenCalled();
+        expect(mockOraFail).toHaveBeenCalledTimes(1);
+    });
+
+    it(`Should properly execute stopDropCollection with log disabled`, function () {
+        const logger = new Logger(mergeOptions({ log: false }));
+        logger.startDropCollection('collection');
+        logger.stopDropCollection(true);
+        expect(mockOra).not.toHaveBeenCalled();
+        expect(mockOraStart).not.toHaveBeenCalled();
+        expect(mockOraSucceed).not.toHaveBeenCalled();
+        expect(mockOraWarn).not.toHaveBeenCalled();
+        expect(mockOraFail).not.toHaveBeenCalled();
+    });
+
+    /* startEmptyCollection */
+
+    it(`Should properly execute startEmptyCollection with log enabled`, function () {
+        const logger = new Logger(mergeOptions({ log: true }));
+        logger.startEmptyCollection('collection');
+        expect(mockOra).toHaveBeenCalledTimes(1);
+        expect(mockOra).toHaveBeenCalledWith({
+            text: `Emptying collection`,
+            spinner: 'dots2'
+        });
+        expect(mockOraStart).toHaveBeenCalledTimes(1);
+    });
+
+    it(`Should properly execute startEmptyCollection with log disabled`, function () {
+        const logger = new Logger(mergeOptions({ log: false }));
+        logger.startEmptyCollection('collection');
+        expect(mockOra).not.toHaveBeenCalled();
+    });
+
+    /* stopEmptyCollection */
+
+    it(`Should properly execute stopEmptyCollection with succeded`, function () {
+        const logger = new Logger(mergeOptions({ log: true }));
+        logger.startEmptyCollection('collection');
+        logger.stopEmptyCollection(true);
+        expect(mockOra).toHaveBeenCalledTimes(1);
+        expect(mockOra).toHaveBeenCalledWith({
+            text: `Emptying collection`,
+            spinner: 'dots2'
+        });
+        expect(mockOraStart).toHaveBeenCalledTimes(1);
+        expect(mockOraSucceed).toHaveBeenCalledTimes(1);
+        expect(mockOraFail).not.toHaveBeenCalled();
+    });
+
+    it(`Should properly execute stopEmptyCollection with failed`, function () {
+        const logger = new Logger(mergeOptions({ log: true }));
+        logger.startEmptyCollection('collection');
+        logger.stopEmptyCollection(false);
+        expect(mockOra).toHaveBeenCalledTimes(1);
+        expect(mockOra).toHaveBeenCalledWith({
+            text: `Emptying collection`,
+            spinner: 'dots2'
+        });
+        expect(mockOraStart).toHaveBeenCalledTimes(1);
+        expect(mockOraSucceed).not.toHaveBeenCalled();
+        expect(mockOraFail).toHaveBeenCalledTimes(1);
+    });
+
+    it(`Should properly execute stopEmptyCollection with log disabled`, function () {
+        const logger = new Logger(mergeOptions({ log: false }));
+        logger.startEmptyCollection('collection');
+        logger.stopEmptyCollection(true);
+        expect(mockOra).not.toHaveBeenCalled();
+        expect(mockOraStart).not.toHaveBeenCalled();
+        expect(mockOraSucceed).not.toHaveBeenCalled();
+        expect(mockOraFail).not.toHaveBeenCalled();
+    });
+
+    /* stopAndClear */
+
+    it(`Should properly execute stopAndClear with logs enabled`, function () {
+        const logger = new Logger(mergeOptions({ log: true }));
+        logger.startDropDatabase('database');
+        logger.stopAndClear();
+        logger.startDropCollection('collection');
+        logger.stopAndClear();
+        logger.startEmptyCollection('collection');
+        logger.stopAndClear();
+        expect(mockOra).toHaveReturnedTimes(3);
+        expect(mockOraStart).toHaveReturnedTimes(3);
+        expect(mockOraStop).toHaveReturnedTimes(3);
+    });
+
+    it(`Should properly execute stopAndClear with logs disabled`, function () {
+        const logger = new Logger(mergeOptions({ log: false }));
+        logger.startDropDatabase('database');
+        logger.stopAndClear();
+        logger.startDropCollection('collection');
+        logger.stopAndClear();
+        logger.startEmptyCollection('collection');
+        logger.stopAndClear();
+        expect(mockOra).not.toHaveBeenCalled();
+        expect(mockOraStart).not.toHaveBeenCalled();
+        expect(mockOraStop).not.toHaveBeenCalled();
+    });
 });
--- test/utils/mockOra.ts
+++ test/utils/mockOra.ts
@@ -1,10 +1,12 @@
 export const mockOraSucceed = jest.fn();
 export const mockOraWarn = jest.fn();
 export const mockOraFail = jest.fn();
+export const mockOraStop = jest.fn();
 export const mockOraStart = jest.fn(() => ({
     succeed: mockOraSucceed,
     warn: mockOraWarn,
-    fail: mockOraFail
+    fail: mockOraFail,
+    stop: mockOraStop
 }));
 export const mockOra = jest.fn(() => ({
     start: mockOraStart
